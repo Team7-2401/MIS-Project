1660f97b623fb37caf2efd7d7fc20611b74001f9
Ali Ishtay <alijpfu0@gmail.com>
2024-12-08 21:33:09 +0300

    Added section7Samples.py for generating sample files to test in section 7.

---
+++ section7Samples.py
@@ -0,0 +1,13 @@
+import testGen
+import pandas as pd
+
+print("starting to generate tests")
+df = pd.DataFrame(columns=['Test', 'Vertices', 'Edge Probability', 'filename'])
+
+for i in range (5, 105, 10):
+    print("i:", i)
+    data = testGen.generateTests(dirname="7", no_of_tests=30, check_size='y', check_prob='n', size=i, prob=[0.02, 0.6])
+    for index, row in data.iterrows():
+        df.loc[len(df)] = [row['Test'], row['Vertices'], row['Edge Probability'], row['filename']]
+
+df.to_csv("./7/results.csv", index=False)
